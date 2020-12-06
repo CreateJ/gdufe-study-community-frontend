6284606b4a277f0893f39a2ece299890fa2c9bae
hardworking-ying <yaying_990623@163.com>
2020-12-06 11:41:08 +0800

    refractor Discuss.vue

--- src/views/Discuss/Discuss.vue
+++ src/views/Discuss/Discuss.vue
@@ -1,221 +1,160 @@
 <template>
   <div id="discuss">
-    <el-row class="discussTitle">
-      <el-col :span="16" :offset="4">
-        <h2 class="title">{{ postTitle }}</h2>
-        <!-- <h2>{{ postId }}</h2> -->
-      </el-col>
-    </el-row>
-    <el-row class="mainBox">
-      <el-col :span="16" :offset="4">
-        <ul class="pd20 userInfo">
-          <li class="userHeader"><img :src="userHeader" alt="" /></li>
-          <li class="userName">{{ userName }}</li>
-          <li class="createTime">{{ createTime }}编辑</li>
-        </ul>
-        <div class="pd20 discrib">详情描述:</div>
-        <div class="pd20 content">{{ postContent }}</div>
+    <el-container>
+      <el-header>
+        <el-row class="discussTitle">
+          <el-col
+            :xs="{ span: 22, offset: 1 }"
+            :sm="{ span: 22, offset: 1 }"
+            :md="{ span: 20, offset: 2 }"
+          >
+            <p class="title">{{ content.title }}</p>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-row class="discussDetail">
+          <el-col
+            :xs="{ span: 19, offset: 1 }"
+            :sm="{ span: 19, offset: 1 }"
+            :md="{ span: 17, offset: 2 }"
+          >
+            <discuss-item 
+              :postman="postman"
+              :content="content"
+              :replies="replies"
+              :replyCount="replyCount"
+              :likeStatus="likeStatus"
+              :likeCount="likeCount"
+            ></discuss-item>
 
-        <ul class="replyBtnBox">
-          <li>
-            <a href="#" @click="clickLike">
-              <img
-                class="icon"
-                :src="
-                  likeStatus == 1
-                    ? require('@/assets/images/icons/love_red.png')
-                    : require('@/assets/images/icons/love_grey.png')
-                "
-                alt=""
-                @click="clickLike(postType,postId,postUserId,postId)"
-              />
-              <span class="likeCount">{{ likeCount }}</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              @click="
-                clickReply(
-                  targetId, //targetId
-                  postId, //entityId
-                  1 //entityType
-                )
-              "
-            >
-              <img class="icon" src="@/assets/images/icons/reply.png" alt="" />
-              <span class="replyCount">{{replyCount }}</span>
-            </a>
-          </li>
-        </ul>
-
-        <!-- <ul class="pd20 replyList"></ul> -->
-        <reply-list class="pd20" :replyListData="replyListData"></reply-list>
-      </el-col>
-    </el-row>
+            <div class="replyRegion" v-show="$store.state.userInfo.user!==undefined">
+              <div class="saySomething">
+                <img :src="userAvatar" alt="" />
+                <span>说点什么吧...</span>
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="3" class="mainBox_right">
+            <button class="replyBtn_right">回复</button>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-import { getPostAllInfo,sendLike } from "@/network/discuss";
-import replyList from "@/views/Discuss/replyList";
-import { OPDE, CODE, URRI} from "@/store/mutations-types";
-import { GRPI } from "@/store/actions-types";
+import { getPostAllInfo } from "@/network/discuss";
+import DiscussItem from "@/views/Discuss/DiscussItem";
 export default {
   name: "",
   components: {
-    replyList
+    DiscussItem,
   },
   props: {},
   data() {
     return {
-      postId: "",
-      postTitle: "",
-      postContent: "",
-      userHeader: "",
-      userName: "",
-      createTime: "",
-      replyCount: "",
-      replyListData: [],
-      targetId: "",
+      postman: {},
+      content: {},
+      replies: [],
+      replyCount: 0,
       likeCount: 0,
       likeStatus: 1,
-      postType: 1,
-      postUserId: "",
     };
   },
   watch: {},
-  computed: {},
+  computed: {
+    userAvatar() {
+      if(this.$store.state.userInfo.user !== undefined) {
+        return this.$store.state.userInfo.user.headerUrl;
+      }
+    }
+  },
   methods: {
     initDiscuss() {
-      this.postId = this.$route.params.postId;
-      getPostAllInfo(this.postId).then(res => {
-        this.postTitle = res.post.title;
-        this.postContent = res.post.content;
-        this.userHeader = res.user.headerUrl;
-        this.userName = res.user.username;
-        this.createTime = res.post.createTime;
+      let postId = this.$route.params.postId;
+      getPostAllInfo(postId).then((res) => {
+        this.postman = res.user;
+        this.content =res.post;
+        this.replies = res.comments;
         this.replyCount = res.post.commentCount;
-        this.replyListData = res.comments;
-        this.targetId = res.user.id;
         this.likeCount = res.likeCount;
         this.likeStatus = res.likeStatus;
-        this.postType = res.post.postType;
-        this.postUserId = res.post.userId;
-        // console.log(res)
       });
     },
-    clickLike(entityType,entityId,entityUserId,postId){
-      // 传入参数 entityType:实体类型;entityId:实体id;entityUserId:实体对应的用户id;postId:文章对应id;
-      sendLike(entityType,entityId,entityUserId,postId).then(res => {
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      })
-    },
-    clickReply(id, entityId, entityType) {
-      this.$store.commit(OPDE);
-      // 请求并更新，当前回复的用户信息，用于显示头像等
-      let postId = this.$route.params.postId; // 获取当前文章id
-      let payload = {
-        userId: id,
-      };
-      let replyRequestInfo = {
-        entityId: entityId,
-        entityType: entityType,
-        postId: postId,
-        targetId: id,
-      }
-      this.$store.commit(URRI,replyRequestInfo)
-      this.$store.dispatch(GRPI, payload);
-    },
+    
   },
   created() {
     this.initDiscuss();
   },
-  mounted() {}
+  mounted() {},
 };
 </script>
 <style scoped>
 #discuss {
-  min-height: 800px;
   background: white;
-  /* padding: 0 20px; */
+}
+#discuss .el-header,
+#discuss .el-main {
+  padding: 0;
 }
 .discussTitle {
-  /* height: 80px; */
   background-color: var(--main-color);
-  background-image: linear-gradient(to right, #80d0c7 0%, #0093e9 100%);
+  background-image: linear-gradient(to right, #80d0c7 0%, #41b2d8 100%);
 }
 .title {
   color: white;
-  padding: 0 20px;
+  font-size: 24px;
+  font-weight: 500;
+  padding: 10px 0;
 }
-.mainBox {
+
+.discussDetail {
   background: white;
 }
-.pd20 {
-  padding: 0 20px;
-}
 .content {
-  margin: 5px 0;
-  font-size: 20px;
+  padding: 15px 0;
 }
-.userInfo {
-  list-style: none;
+
+.mainBox_right {
+  position: fixed;
+  right: 100px;
+  top: 200px;
+  padding-left: 30px;
 }
-.userInfo li {
-  display: inline-block;
-  margin-right: 5px;
+.replyBtn_right {
+  color: #fff;
+  letter-spacing: 0.2em;
+  padding: 5px 20px;
+  border: 1px solid var(--main-color);
+  border-radius: 2px;
+  box-shadow: 0 0 8px 2px rgb(233, 233, 233);
+  background-color: var(--main-color);
 }
-.userHeader {
-  width: 25px;
-  border-radius: 3px;
+.replyRegion {
+  margin: 20px 0;
+  padding: 20px 0;
+  border: 2px dashed transparent;
+  border-radius: 6px;
+  cursor: text;
 }
-.userHeader img {
-  width: 25px;
+.replyRegion:hover {
+  border: 2px dashed #ccc;
 }
-.userName {
-  font-weight: bolder;
-  font-size: 20px;
-  line-height: 20px;
+.saySomething {
+  padding-bottom: 4px;
+  border-bottom: 1px solid #ccc;
 }
-.createTime {
-  font-size: 14px;
-  color: rgba(0, 0, 0, 0.5);
-}
-.discrib {
-  font-size: 26px;
-  font-weight: bolder;
-}
-.replyBtnBox {
-  /* display: relative; */
-  widows: 100%;
-  margin: 5px 20px 0 5px;
-}
-.replyBtnBox li {
-  /* display: inline-block; */
-  float: right;
-}
-a,
-a:link,
-a:visited,
-a:hover,
-a:active {
-  text-decoration: none;
-  color: black;
-  margin-right: 20px;
-}
-.icon {
+.saySomething img {
   width: 20px;
   height: 20px;
+  border-radius: 3px;
+  margin-right: 5px;
 }
-.likeCount,
-.replyCount {
-  height: 14px;
-  width: 20px;
-  font-size: 16px;
-  color: #777;
+.saySomething span {
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.5);
   vertical-align: top;
 }
 </style>
