7780de7a9ed3eb4d39090fc40c594c05dbb5f5da
王驰捷 <905260054@qq.com>
2020-12-20 12:47:01 +0800

    首页添加头像

---
+++ src/views/Info/info.vue
@@ -0,0 +1,29 @@
+<template>
+  <div><el-row>
+    <el-col :span="16" :offset="4">
+
+      <h2>消息页面
+      </h2>
+    </el-col>
+  </el-row></div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  props: {},
+  data() {
+    return {
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style scoped>
+
+</style>
