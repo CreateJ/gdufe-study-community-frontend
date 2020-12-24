a3d2e639d22dfd997100bd28468377c30248f5cb
hardworking-ying <yaying_990623@163.com>
2020-12-25 01:39:03 +0800

    回到顶部

---
+++ src/components/scroll/scrollToTop.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="scroll-btn" @click="toTop">
+    <i class="el-icon-top" alt="返回顶部"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScrollTop",
+  methods: {
+    toTop() {
+      window.scrollTo(0, 0);
+    }
+  }
+}
+</script>
+
+<style>
+.scroll-btn {
+  position: fixed;
+  bottom: 100px;
+  right: 40px;
+  width: 40px;
+  height: 40px;
+  color: #FFF;
+  line-height: 40px;
+  text-align: center;
+  background-color: var(--main-color);
+  border-radius: 50%;
+  cursor: pointer;
+}
+.scroll-btn:hover {
+  opacity: .8;
+}
+</style>
